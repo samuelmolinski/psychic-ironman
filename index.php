fd9f71143812efbaa68bb33a742789b85c871f44
Samuel Molinski <samuel.molinski@creativefocus.com.br>
2012-09-27 16:45:36 -0300

    -yiic unedit -added pages

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 // change the following paths if necessary
+//$yii=dirname(__FILE__).'/../Yii/framework/yii.php';
 $yii=dirname(__FILE__).'/yii_1.1.12/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
--- protected/controllers/Fb_pageController.php
+++ protected/controllers/Fb_pageController.php
@@ -9,34 +9,15 @@
 
 		public function actionEspalhe() {
 			$this -> render('espalhe');
+		}		
+		
+		public function actionFanpage() {
+			if($this->authenticate()){
+				$this -> render('fanpage');
+			} else {
+				$this -> redirect('index');
+			}
 		}
-
-		// -----------------------------------------------------------
-		// Uncomment the following methods and override them if needed
-		/*
-		 public function filters()
-		 {
-		 // return the filter configuration for this controller, e.g.:
-		 return array(
-		 'inlineFilterName',
-		 array(
-		 'class'=>'path.to.FilterClass',
-		 'propertyName'=>'propertyValue',
-		 ),
-		 );
-		 }
-
-		 public function actions()
-		 {
-		 // return external action classes, e.g.:
-		 return array(
-		 'action1'=>'path.to.ActionClass',
-		 'action2'=>array(
-		 'class'=>'path.to.AnotherActionClass',
-		 'propertyName'=>'propertyValue',
-		 ),
-		 );
-		 }*/
 
 		public function authenticate() {
 
