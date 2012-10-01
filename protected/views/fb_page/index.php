1aab9b7126380d55cd7561052c182044fbe24fca
Samuel Molinski <samuel.molinski@creativefocus.com.br>
2012-10-01 17:00:00 -0300

    -more layout changes

--- protected/views/fb_page/index.php
+++ protected/views/fb_page/index.php
@@ -1,54 +1,37 @@
 <?php
 	$this -> breadcrumbs = array('index', );
 
-	Yii::app() -> facebook -> ogTags['title'] = "My Page Title";
+	//Yii::app() -> facebook -> ogTags['title'] = "My Page Title";
 ?>
-<h1>Index</h1>
-
-<p>You may change the content of this page by modifying the file <tt><?php echo __FILE__; ?></tt>.</p>
+<div id="logoVideo" class="clearfix">
+	<div class="cutir">
+		<img class="msf-logo" src="<?php echo Yii::app()-> params['root']; ?>/images/logo-mediocsSemFronteiras.png">
+		<img class="msf-curtir" src="<?php echo Yii::app()-> params['root']; ?>/images/logo-poderDoSeuCurtir.png">
+	</div>
+	<div class="mainVideo">
+		<iframe width="485" height="296" src="http://www.youtube.com/embed/g63uwgbAbZE" frameborder="0" allowfullscreen></iframe>
+	</div>
+</div>
+<div id="network" class="clearfix">
+	<div class="rede"><img class="" src="<?php echo Yii::app()-> params['root']; ?>/images/logo-msfRede.png"></div>
+	<div class="underMainVideo">
+		<div class="auth">
+			<div class="btn connectFacebook"></div>
+		</div>
+		<div class="curtirCount">
+			<div class="count"><span class="totalLikesContainer">+&nbsp;<span class="totalLikes">134,302</span></span></div>			
+			<div class="btn curtir"></div>
+		</div>
+	</div>
+</div>
+<div id="videos" class="clearfix">
+	
+</div>
+<div id="callToAction" class="clearfix">
+	
+</div>
 
 <?php ?>
 
-<?php
-
-	$loginUrl = $this->getFbLoginUrl();
-	//d($loginUrl);			
-	//d($auth);
-	if ($auth) {
-		//time limit for 90 days
-		$timeUntil = time() - (3600 * 24 * 90);
-		$user_feed = Yii::app() -> facebook -> api("/me/feed?until=$timeUntil&metadata=1&limit=500");
-		$user_friends = Yii::app() -> facebook -> api("/me/friends");
-		//get only post with comments
-		$commentFeed = array();
-		$mostComments = array();
-		foreach ($user_feed['data'] as $k => $post) {
-			//d($post);
-			//d($post['comments']['count']);
-			if ($post['comments']['count'] != 0) {
-				array_push($commentFeed, $post);
-			}
-		}
-		foreach($commentFeed as $k=>$comments){
-			if($comments['comments']['data']){
-				foreach($comments['comments']['data'] as $j=>$comment){
-					if($auth['id']!=$comment['from']['id']){
-						if(!key_exists($comment['from']['id'], $mostComments)) {
-							$mostComments[$comment['from']['id']] = 1;
-						} else {
-							$mostComments[$comment['from']['id']] = $mostComments[$comment['from']['id']]+1;
-						}
-					}
-				}
-			}
-		}
-		$appData = $this->getAppData();
-		d($appData);
-		d($mostComments);
-		//d($user_feed);
-		d($user_friends);
-
-		$test = $auth;
-	}//d($test);
-?><br />
-<a id='userAuth' href='#' onclick='setTimeout(function() {top.location.href = "<?php echo $loginUrl; ?>"}, 500);'>Authorize app</a>
+<br />
+<a id='userAuth' href='#' onclick='setTimeout(function() {top.location.href = "<?php //echo $loginUrl; ?>"}, 500);'>Authorize app</a>
